e77d6fabeaea2a69ac48327e9720531acf7f3657
StasH <root0000@gmail.com>
2010-12-09 16:47:44 +0300

    Реализован контроллер навигации UINavigationController

--- PageDemo/Classes/PageDemoAppDelegate.h
+++ PageDemo/Classes/PageDemoAppDelegate.h
@@ -5,12 +5,18 @@
 //--------------------------------------------------------------------------
 
 #import <UIKit/UIKit.h>
+#import "RootViewController.h"
+#import "CreditsViewController.h"
 
 
 @interface PageDemoAppDelegate : NSObject <UIApplicationDelegate> 
 {
     UIWindow *window;
+    RootViewController *rootViewController;
+    CreditsViewController *creditsViewController;
+    UINavigationController *navigationController;
 }
+
 
 @end
 
--- PageDemo/Classes/PageDemoAppDelegate.m
+++ PageDemo/Classes/PageDemoAppDelegate.m
@@ -13,6 +13,14 @@
 
 - (void) applicationDidFinishLaunching: (UIApplication *) application
 {    
+    window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    
+    rootViewController = [[RootViewController alloc] initWithAppDelegate: self];
+    creditsViewController = [[CreditsViewController alloc] initWithAppDelegate: self];
+    
+    navigationController = [[UINavigationController alloc] initWithRootViewController: rootViewController];
+    
+    [window addSubview: [navigationController view]];
     
     [window makeKeyAndVisible];
 }
@@ -20,9 +28,24 @@
 
 - (void)dealloc 
 {
+    [navigationController release];
+    [rootViewController release];
+    [creditsViewController release];
 
     [window release];
     [super dealloc];
+}
+
+
+-(void) credits
+{
+    [navigationController pushViewController: creditsViewController animated: YES];
+}
+
+
+-(void) back
+{
+    [navigationController popViewControllerAnimated: YES];
 }
 
 
