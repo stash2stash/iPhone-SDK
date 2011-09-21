5b454331a6e2111aafe6f208ff0bcc2064c23f61
StasH <root0000@gmail.com>
2011-09-21 11:47:10 +0400

    Пример обработки касаний, движение спрайта

---
+++ TouchDemo2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TouchDemo2
+//
+//  Created by StasH on 20.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"TouchDemo2AppDelegate");
+    [pool release];
+    return retVal;
+}
