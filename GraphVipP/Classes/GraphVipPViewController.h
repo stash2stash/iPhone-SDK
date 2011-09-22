aa15299d666619ec681be9eeaa44cb8eec8beabe
StasH <root0000@gmail.com>
2011-09-22 20:23:04 +0400

    Делаем отображения графика ВИП и П

---
+++ GraphVipP/Classes/GraphVipPViewController.h
@@ -0,0 +1,19 @@
+//
+//  GraphVipPViewController.h
+//  GraphVipP
+//
+//  Created by StasH on 22.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+
+@interface GraphVipPViewController : UIViewController 
+{
+ //   CALayer *gridLayer;
+}
+
+@end
+
---
+++ GraphVipP/Classes/GraphVipPViewController.m
@@ -0,0 +1,53 @@
+//
+//  GraphVipPViewController.m
+//  GraphVipP
+//
+//  Created by StasH on 22.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphVipPViewController.h"
+
+
+@implementation GraphVipPViewController
+
+/*
+- (id) init
+{
+    self = [super init];
+    if (self != nil) {
+        gridLayer = [CALayer layer];
+        
+    }
+    return self;
+}
+*/
+
+- (void) viewDidLoad
+{
+    [self viewDidLoad];
+    
+   // self.view.layer.cornerRadius = 10;
+    //self.view.layer.backgroundColor = [UIColor orangeColor].CGColor; //[UIColor colorWithRed:249.0/255.0 green:237.0/255.0 blue:219.0/255.0 alpha:1.0].CGColor;
+    
+    
+    //[self.view.layer addSublayer: gridLayer];
+    
+    //[background needsDisplay];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning 
+{
+    [super didReceiveMemoryWarning];
+}
+
+
+- (void)dealloc 
+{
+    [super dealloc];
+}
+
+@end
